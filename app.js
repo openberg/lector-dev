65f27682f125f8c305b8d51c1b10247d3f7fa75e
David Rajchenbach-Teller <Yoric@users.sf.net>
2014-10-18 11:44:38 +0200

    Columns

--- app.js
+++ app.js
@@ -29,30 +29,34 @@ try {
       console.log("Chapters", book.chapters);
       var promise = book.chapters[chapterNum].asXML();
       promise = promise.then(xml => {
+        //
+        // Adapt XML document for proper display.
+        //
         var head = xml.querySelector("html > head");
+
+        // 1. Inject book stylesheet
         var link = xml.createElement("link");
         link.setAttribute("rel", "stylesheet");
         link.setAttribute("type", "text/css");
         link.setAttribute("href", UrlUtils.toURL("css/books.css"));
         head.appendChild(link);
 
-/*
-        var style = xml.createElement("style");
-        style.textContent = "body { background-color:red; }";
-        head.appendChild(style);
-*/
-        // FIXME: Inject proper <style>
-        // (link rel = stylesheet?)
-
-        // FIXME: Inject <script>
-
-        // FIXME: Split this across several ticks.
-        var source = new XMLSerializer().serializeToString(xml);
-        var encoded = new TextEncoder().encode(source);
+        // FIXME: Do we need to inject <script>?
+        // FIXME: We probably want to rewrite all links
+        // (for images, stylesheets, etc.)
+        return xml;
+      });
+      promise = promise.then(xml => {
+        return Promise.resolve(new XMLSerializer().serializeToString(xml));
+      });
+      promise = promise.then(source => {
+        return Promise.resolve(new TextEncoder().encode(source));
+      });
+      promise = promise.then(encoded => {
         var blob = new Blob([encoded], { type: "text/html" }); 
         var url = URL.createObjectURL(blob);
         $("contents").setAttribute("src", url);
-        // FIXME: revokeObjectURL (can we do this immediately?)
+        URL.revokeObjectURL(url);
       });
       promise = promise.then(null, e => console.error(e));
     });
