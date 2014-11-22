7b8744e71a5380128e1923b862e30e01612ba9cf
David Rajchenbach-Teller <Yoric@users.sf.net>
2014-11-22 16:06:24 +0100

    Testing filepicker, part 7

--- js/filepicker.js
+++ js/filepicker.js
@@ -62,7 +62,7 @@ FilePicker.prototype = {
     var activity = new MozActivity({
       name: "pick",
       data: {
-        type: this._mimetypes[0]
+        type: "application/*"
       }
     });
     activity.onsuccess = () => {
