6a019f4350eec66ecab498665bde2427f13d5eba
fatimzahara <zahara.kone@gmail.com>
2014-12-15 17:10:11 +0100

    Issue #138 Add a variable in 'Config'
    Add a variable in 'Config' to determine whether we should go to
    fullscreen.

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ window.require(['js/polyfills',
                 'js/book',
                 'js/bookviewer',
                 'js/book-epub.js',
+                'js/config',
                 'js/filepicker',
                 'js/library',
                 'js/menu',
                 'js/urlutils'],
-  function(_, Book, BookViewer, BookEPub, FilePicker, Library, Menu, UrlUtils) {
+  function(_, Book, BookViewer, BookEPub, Config, FilePicker, Library, Menu, UrlUtils) {
 "use strict";
 
 var $ = id => document.getElementById(id);
@@ -150,7 +151,9 @@ library.init().then(() => {
       //
       //If the user clicks on a book, it loads in mode fullscreen
       //
-      document.body.mozRequestFullScreen();
+      if (Config.allowFullScreen){
+        document.body.mozRequestFullScreen();
+      }
     });
     libraryElement.insertBefore(li, $("pick"));
 
